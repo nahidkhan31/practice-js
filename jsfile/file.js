de55fa5187bbf8561ec7a9efeece794755de7949
nahidkhan31 <nahidkhanniloy31@.com>
2025-02-24 00:50:43 +0600

    addedcode

--- jsfile/file.js
+++ jsfile/file.js
@@ -1,1 +1,17 @@
-console.log("connected");
+// // step number 01
+// document.getElementById('btn-post-comment').addEventListener('click', function(){
+    
+// // step number 02: get the text written in the comment text area
+// const commentTextBox = document.getElementById('comment-text-box');
+// const newComment= commentTextBox.value;
+
+// // step-03: get the parent node where to publish comment
+// const commentContainer= document.getElementById('comment-container');
+
+// // step-04: create a comment p and set the innerText
+// const  commentElement = document.createElement('p');
+// commentElement.innerText = newComment;
+
+// // step-05:  append the new p tag to the parent node
+// commentContainer.appendChild(commentElement);
+// })
